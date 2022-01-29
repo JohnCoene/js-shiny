83607e4a5aa8489e3e01d3868d3aa4261c3afe11
John (JP) Coene <jcoenep@gmail.com>
2022-01-29 08:54:01 +0100

    fix: better random inputs

--- data.R
+++ data.R
@@ -6,37 +6,33 @@ make_data <- function(n = sample(5:50, 1)) {
 }
 
 # pick random item
-rand <- function() {
-  fn <- sample(c("item1", "item2", "item3"), 1)
-  do.call(fn, list())
+rand <- function(n = sample(1:4, 1)) {
+  lapply(seq(n), \(x){
+    fn <- sample(c("item1", "item2", "item3"), 1)
+    do.call(fn, list())
+  })
 }
 
 # for a text input
 item1 <- function(n = sample(1:3, 1)) {
-  lapply(seq(n), \(x){
-    list(
-      type = "text",
-      value = paste0(sample(letters, 10), collapse = "")
-    )
-  })
+  list(
+    type = "text",
+    value = paste0(sample(letters, 10), collapse = "")
+  )
 }
 
 # for a select input
 item2 <- function(n = sample(1:3, 1)) {
-  lapply(seq(n), \(x) {
     list(
       type = "select",
       value = sample(LETTERS[1:4], 1)
     )
-  })
 }
 
 # for a numeric input
 item3 <- function(n = sample(1:3, 1)) {
-  lapply(seq(n), \(x) {
-    list(
-      type = "number",
-      value = sample(1:100, 1)
-    )
-  })
+  list(
+    type = "number",
+    value = sample(1:100, 1)
+  )
 }
